aadd052749fb0a4efa8cc16937facb81255d3022
Odenktools <odenktools86@gmail.com>
2020-03-20 15:52:27 +0700

    add oauth2 step#5

---
+++ oauth/service_interface.go
@@ -0,0 +1,49 @@
+package oauth
+
+import (
+	"go-echo-api/entity"
+	"go-echo-api/config"
+	"go-echo-api/session"
+	"go-echo-api/utils/routes"
+	"github.com/labstack/echo"
+	"github.com/jinzhu/gorm"
+	"go-oauth2-server/models"
+)
+
+// ServiceInterface defines exported methods
+type ServiceInterface interface {
+	// Exported methods
+	GetConfig() *config.Config
+	RestrictToRoles(allowedRoles ...string)
+	IsRoleAllowed(role string) bool
+	FindRoleByID(id string) (*entity.OauthRole, error)
+	GetRoutes() []routes.Route
+	RegisterRoutes(router *echo.Router, prefix string)
+	ClientExists(clientID string) bool
+	FindClientByClientID(clientID string) (*entity.OauthClient, error)
+	CreateClient(clientID, secret, redirectURI string) (*entity.OauthClient, error)
+	CreateClientTx(tx *gorm.DB, clientID, secret, redirectURI string) (*entity.OauthClient, error)
+	AuthClient(clientID, secret string) (*entity.OauthClient, error)
+	UserExists(username string) bool
+	FindUserByUsername(username string) (*entity.OauthUser, error)
+	CreateUser(roleID, username, password string) (*entity.OauthUser, error)
+	CreateUserTx(tx *gorm.DB, roleID, username, password string) (*entity.OauthUser, error)
+	SetPassword(user *entity.OauthUser, password string) error
+	SetPasswordTx(tx *gorm.DB, user *entity.OauthUser, password string) error
+	UpdateUsername(user *entity.OauthUser, username string) error
+	UpdateUsernameTx(db *gorm.DB, user *entity.OauthUser, username string) error
+	AuthUser(username, thePassword string) (*entity.OauthUser, error)
+	GetScope(requestedScope string) (string, error)
+	GetDefaultScope() string
+	ScopeExists(requestedScope string) bool
+	Login(client *models.OauthClient, user *entity.OauthUser, scope string) (*entity.OauthAccessToken, *entity.OauthRefreshToken, error)
+	GrantAuthorizationCode(client *entity.OauthClient, user *entity.OauthUser, expiresIn int, redirectURI, scope string) (*entity.OauthAuthorizationCode, error)
+	GrantAccessToken(client *entity.OauthClient, user *entity.OauthUser, expiresIn int, scope string) (*entity.OauthAccessToken, error)
+	GetOrCreateRefreshToken(client *entity.OauthClient, user *entity.OauthUser, expiresIn int, scope string) (*entity.OauthRefreshToken, error)
+	GetValidRefreshToken(token string, client *entity.OauthClient) (*entity.OauthRefreshToken, error)
+	Authenticate(token string) (*entity.OauthAccessToken, error)
+	//NewIntrospectResponseFromAccessToken(accessToken *models.OauthAccessToken) (*IntrospectResponse, error)
+	//NewIntrospectResponseFromRefreshToken(refreshToken *models.OauthRefreshToken) (*IntrospectResponse, error)
+	ClearUserTokens(userSession *session.UserSession)
+	Close()
+}
